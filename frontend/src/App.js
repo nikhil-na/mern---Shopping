88187eed9b24eb2392bf2e8ffbbe9c9fa4cb7357
Nikhil Aryal <aryaln0058@gmail.com>
2023-11-16 20:34:52 -0600

    Formatted the frontend files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,25 @@
-import './App.css';
-import Home from './components/Home';
-import Footer from './components/layot/Footer';
-import Header from './components/layot/Header';
+import "./App.css";
+import Home from "./components/Home";
+import Footer from "./components/layot/Footer";
+import Header from "./components/layot/Header";
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import ProductDetails from './components/product/ProductDetails';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProductDetails from "./components/product/ProductDetails";
 
 function App() {
   return (
-    
-      <BrowserRouter>
-        <Header />
-          <div className="App">
-            <div className='container container-fluid'>
-              <Routes>
-                <Route path = '/' element = {<Home />} exact/>
-                <Route path = '/product/:id' element = {<ProductDetails />} exact/>
-              </Routes>
-              </div>
-          </div>
-        <Footer />
-      </BrowserRouter>
-      
+    <BrowserRouter>
+      <Header />
+      <div className="App">
+        <div className="container container-fluid">
+          <Routes>
+            <Route path="/" element={<Home />} exact />
+            <Route path="/product/:id" element={<ProductDetails />} exact />
+          </Routes>
+        </div>
+      </div>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
--- frontend/src/components/layot/Footer.jsx
+++ frontend/src/components/layot/Footer.jsx
@@ -1,15 +1,15 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from "react";
 
 function Footer() {
   return (
     <Fragment>
-        <footer className="py-1">
-            <p className="text-center text-black mt-1">
-            &copy; Shopper - 2023, All Rights Reserved
-            </p>
-        </footer>
+      <footer className="py-1">
+        <p className="text-center text-black mt-1">
+          &copy; Shopper - 2023, All Rights Reserved
+        </p>
+      </footer>
     </Fragment>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
